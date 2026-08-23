aaeae7a3e909ec1e7fa671c6ce4b709f00985441
unknown <ashivakumar688@gamil.com>
2026-08-23 06:43:25 +0530

    provider.tf sunday

---
+++ provider.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Microsoft Entra Tenant ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure App/Service Principal Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  sensitive   = true
+  description = "Azure App/Service Principal Client Secret"
+}
