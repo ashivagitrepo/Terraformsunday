64568f35ca7612f09bc8aae373873c36d04ca265
ashivagitrepo <ashivakumar688@gmail.com>
2026-08-25 13:32:38 +0530

    Clean up comments in provider.tf
    Removed multiple comment lines for clarity.

--- provider.tf
+++ provider.tf
@@ -36,14 +36,7 @@ variable "client_secret" {
   sensitive   = true
   description = "Azure App/Service Principal Client Secret"
 }
-adding one line in comments to test in git required_providers
-adding one more line to test again in gogit repo
-adding 3rd time from repo to checck in local and main branch
-adding 4th time in local
-adding 5th time from git to local 
-adding 6th time from local to git repo with colne from repo link
-dding 7th time from gitrepo to test main and local 
-adding new lines today monday 24th August
-testing from gitrepo to main and them to local repo 
+# deleted 6 lines 
+
 
 
